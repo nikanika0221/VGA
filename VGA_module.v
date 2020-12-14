ff298c51ad1e1b2bc5665e332126fdd75dbaa00b
nikanika0221 <60596671+nikanika0221@users.noreply.github.com>
2020-12-14 17:50:30 +0400

    Update VGA_module.v

--- VGA_module.v
+++ VGA_module.v
@@ -1,13 +1,12 @@
 `timescale 1ns / 1ps
 module VGA_module(
  input clk,
- input sw,
  output reg Horizonatalsync = 0, 
  output reg Verticalsync = 0,
  output reg [3:0] red ,
  output reg[3:0] green ,
  output reg[3:0] blue,
-output reg LED
+ input [2:0] controllers //rgb on-off
  );
   
 //assign LED = sw;
@@ -15,51 +14,57 @@ output reg LED
  wire enablev;
  wire[15:0] hcount; 
  wire[15:0] vcount; 
- 
- 
+   
  Hor_Counter horis (
-     .clk_slow(clk_slow),
      .enablev(enablev),
      .hcount(hcount),
-	  .clk(clk)
+	  .clk(clk),
+	  .clk_slow(clk_slow)
  );
  
- Vertical_Counter vertic (
-     .clk_slow(clk_slow),
+ Ver_Counter vertic (
      .enablev(enablev),
      .vcount(vcount),
-	  .clk(clk)
+	  .clk(clk)	  
  );
-
-
 	always @(posedge clk_slow) begin 
-    
-        if(sw)begin
-        LED = 0;
-                Horizonatalsync= ( hcount <128) ? 1'b1:1'b0;
-                Verticalsync= ( vcount <4) ? 1'b1:1'b0;
-            
-                red = ( hcount <1140 && hcount >180&& vcount <665 && vcount>30) ? 15:6;
-                blue =( hcount <1140 && hcount >180 && vcount <665 && vcount>30) ? 15:6;
-                green =( hcount <1140  && hcount >180 && vcount <665 && vcount>30) ? 15:6;
-                end
-
-		else begin 
-		LED = 1;
-                  Horizonatalsync= ( hcount <128) ? 1'b1:1'b0;
-                  Verticalsync= ( vcount <4) ? 1'b1:1'b0;
-	
-	             //  if(hcount <1140 && hcount >180&& vcount <665 && vcount>30) 
-                 //  begin
-                  //  red = 15; blue = 0; green = 0;
-                  // end 
-			red= ( hcount <1040 && hcount >180&& vcount <665 && vcount>30) ? 15:0;
-			blue=( hcount <1040 && hcount >180 && vcount <665 && vcount>30) ? 1:0;
-			green=( hcount <1040  && hcount >180 && vcount <665 && vcount>30) ? 1:0;
-			end
-		
+        
+         Horizonatalsync = ( hcount <128) ? 1 : 0;
+         Verticalsync= ( vcount <4) ? 1'b1:1'b0;
+         
+        //write T
+        if((((hcount > 300 && hcount < 500)&& (vcount > 200 && vcount < 250))
+        || ((hcount > 375 && hcount < 425) && (vcount > 250 && vcount < 450)))&& controllers[0])
+        begin
+            red = 0;
+            green = 15;
+            blue = 0;
+        end
+        
+        //write H
+        else if(((((hcount > 550 && hcount < 600) || (hcount > 700 && hcount < 750))  && (vcount > 200 && vcount < 450)) 
+        || ((hcount > 600 && hcount < 700) && (vcount > 300 && vcount < 350)))&& controllers[1])
+        begin
+            red = 15;
+            green = 0;
+            blue = 0;
+        end
+        
+         //write E
+         else if((((hcount > 800 && hcount < 850) && (vcount > 200 && vcount < 450))
+         || ((hcount > 850 && hcount < 975)&&((vcount > 400 && vcount < 450)||(vcount > 300 && vcount < 350)||(vcount > 200 && vcount < 250))))&& controllers[2])
+               begin
+                   red = 0;
+                   green = 0;
+                   blue = 15;
+               end
+                 
+        else
+        begin
+            red = 0;
+            green = 0;
+            blue = 0;
+			  
+		  end		
 	end
-
-
-
 endmodule
